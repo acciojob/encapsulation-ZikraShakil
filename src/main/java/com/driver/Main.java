815eb7f99b13658822807223766c7e6b4e8dafac
zikra <zikraparween70@gmail,com>
2023-06-08 21:19:08 +0530

    I added some

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Zikra");
+        String name =rwOnly.getName();
+    }
   
 }
